1ea1f8a7c9c9849134f64aadfb66d6496d51119a
Vishal bala <vishal.bala.100@gmail.com>
2025-01-12 21:11:26 +0530

    hoola

--- src/index.js
+++ src/index.js
@@ -72,20 +72,20 @@ const io = new Server(server, {
 });
 
 // Socket.IO Middleware for Authentication
-io.use((socket, next) => {
-  const token = socket.handshake.auth.token;
-  if (!token) {
-    return next(new Error("Authentication error: No token provided"));
-  }
+// io.use((socket, next) => {
+//   const token = socket.handshake.auth.token;
+//   if (!token) {
+//     return next(new Error("Authentication error: No token provided"));
+//   }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return next(new Error("Authentication error: Invalid token"));
-    }
-    socket.user = decoded;
-    next();
-  });
-});
+//   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+//     if (err) {
+//       return next(new Error("Authentication error: Invalid token"));
+//     }
+//     socket.user = decoded;
+//     next();
+//   });
+// });
 
 // Socket.IO Event Handling
 io.on("connection", (socket) => {
